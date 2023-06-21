d20907a2b0874247f5b2fb8f2187fa477648356d
Macos <mohammed.kamal@tiu.edu.iq>
2023-06-20 23:17:13 -0400

    Form using userFer

---
+++ RP_9_form/src/App.tsx
@@ -0,0 +1,12 @@
+// import Form from "./Form";
+import FormUseRef from "./FormUseRef";
+const App = () => {
+ return (
+  <>
+   {/* <Form />; */}
+   <FormUseRef />
+  </>
+ );
+};
+
+export default App;
