a772ad0b0556dae8a1fbd91ea0b8d401d5a151f9
Macos <mohammed.kamal@tiu.edu.iq>
2023-06-22 22:20:48 -0400

    Validation added

--- RP_9_form/src/FormUsingReactHook.tsx
+++ RP_9_form/src/FormUsingReactHook.tsx
@@ -1,9 +1,21 @@
 import { useForm } from "react-hook-form";
 
-const FormUsingReactHook = () => {
- const { register, handleSubmit } = useForm();
+interface FormData {
+ name: string;
+ age: number;
+}
 
- console.log(register("name"));
+const FormUsingReactHook = () => {
+ const {
+  register,
+  handleSubmit,
+  formState: { errors },
+ } = useForm<FormData>();
+ //  console.log("Register: " + register);
+ //  console.log("handleSubmit: " + handleSubmit);
+ console.log(errors);
+
+ //  console.log(register("name"));
  return (
   <>
    <form onSubmit={handleSubmit((data) => console.log(data))}>
@@ -12,22 +24,29 @@ const FormUsingReactHook = () => {
       Name
      </label>
      <input
-      {...register("name")}
+      {...register("name", { required: true, minLength: 3, maxLength: 12 })}
       id="name"
       type="text"
       className="form-control"
      />
+     {errors.name?.type === "required" && <p> name is required</p>}
+     {errors.name?.type === "minLength" && (
+      <p> Enter more than two alphabets</p>
+     )}
     </div>
     <div className="mb-3">
      <label htmlFor="age" className="form-label">
       Name
      </label>
      <input
-      {...register("age")}
+      {...register("age", { required: true, min: 1, max: 150 })}
       id="age"
       type="number"
       className="form-control"
      />
+     {errors.age?.type === "required" && <p>Age is required</p>}
+     {errors.age?.type === "min" && <p>Age Shoulde be more than 0</p>}
+     {errors.age?.type === "max" && <p>Age Shoulde not exceed 150</p>}
     </div>
     <button className="btn btn-primary" type="submit">
      Submit
