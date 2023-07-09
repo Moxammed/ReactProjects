06244a1c8d10cbb19b81e76fd6899538a1d1d4de
Macos <mohammed.kamal@tiu.edu.iq>
2023-07-08 22:28:58 -0400

    spinner + deleting data or user

--- RP_14_FetchDataFromServer/src/FetchData.tsx
+++ RP_14_FetchDataFromServer/src/FetchData.tsx
@@ -7,34 +7,65 @@ interface User {
 }
 
 const FetchData = () => {
- const [user, setUser] = useState<User[]>([]);
+ const [users, setUser] = useState<User[]>([]);
  const [error, setError] = useState("");
+ const [isLoading, setLoading] = useState(false);
 
  useEffect(() => {
+  setLoading(true);
   const controller = new AbortController();
+  setLoading(true);
   axios
    .get("https://jsonplaceholder.typicode.com/users", {
     signal: controller.signal,
    })
    //    .then((response) => console.log(response.data))
-   .then((response) => setUser(response.data))
+   .then((response) => {
+    setUser(response.data);
+    setLoading(false);
+   })
    .catch((err) => {
     if (err instanceof CanceledError) return;
     setError(err.message);
+    setLoading(false);
    });
 
   return () => controller.abort();
  }, []);
 
+ const deleteUser = (user: User) => {
+  const originalUser = [...users];
+  setUser(users.filter((u) => u.id !== user.id));
+
+  axios
+   .delete("https://jsonplaceholder.typicode.com/users/" + user.id)
+   .catch((err) => {
+    setError(err.message);
+    setUser(originalUser);
+   });
+ };
+
  return (
   <>
    {error && <p className="text-danger">{error}</p>}
-   <ol>
+   {isLoading && <div className="spinner-border"></div>}
+   <ul className="list-group">
     {" "}
-    {user.map((userr) => (
-     <li key={userr.id}>{userr.name}</li>
+    {users.map((userr) => (
+     <li
+      key={userr.id}
+      className="list-group-item d-flex justify-content-between"
+     >
+      {userr.name}{" "}
+      <button
+       className="btn btn-outline-danger"
+       onClick={() => deleteUser(userr)}
+      >
+       Delete
+      </button>
+     </li>
     ))}
-   </ol>
+   </ul>
   </>
  );
 };
