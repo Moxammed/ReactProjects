6a272241ddadf47e96bc2384fc7b12bb13d989f7
Macos <mohammed.kamal@tiu.edu.iq>
2023-07-24 00:10:44 -0400

    Reading progress in a page

---
+++ ProgressReadingBar/src/App.tsx
@@ -0,0 +1,98 @@
+import React from "react";
+import UseReadingProgress from "./UseReadingProgress";
+
+const App = () => {
+ const completion = UseReadingProgress();
+ return (
+  <>
+   <nav className="sticky z-50 top-0 backdrop-blur-3xl py-2">
+    <span
+     id="progress-bar"
+     style={{
+      transform: `translateX(${completion - 100}%)`,
+     }}
+     className={`absolute bottom-0 w-full transition-transform duration-150 h-1 bg-yellow-400`}
+    />
+   </nav>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+   <div>sdfsdf</div>
+  </>
+ );
+};
+
+export default App;
